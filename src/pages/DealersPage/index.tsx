f8f68653aa0ebb00dcb1eaea940a163b75f14af1
aleksejpetrovec <rbtechologics@mail.ru>
2024-02-21 21:53:53 +0300

    добавлены фильтры по городам;

--- src/pages/DealersPage/index.tsx
+++ src/pages/DealersPage/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useMemo, useState } from "react";
+import { ChangeEvent, FC, useEffect, useMemo, useState } from "react";
 import styles from "./DealersPage.module.scss";
 import { HeaderSlider } from "components/HeaderCarousel";
 import { YMaps, Map, Placemark } from "react-yandex-maps";
@@ -8,23 +8,97 @@ import { CommonButton, LinkButton } from "components/common/Buttons";
 import { Construction, DirectionsCar, NoCrash } from "@mui/icons-material";
 import { Path } from "enum/PathE";
 import { generatePath } from "react-router-dom";
+import { DealersPageT } from "api/apiTypes";
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiRadio: {
+      styleOverrides: {
+        root: {
+          "&.Mui-checked": {
+            color: "#cf2626", // Измените цвет на свой выбранный
+          },
+        },
+      },
+    },
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          color: "black",
+          "&.Mui-focused": {
+            color: "black",
+          },
+          fontSize: "20px",
+        },
+      },
+    },
+  },
+});
+
+const mapEndpoints: {
+  city: string;
+  center: number[];
+  zoom: number;
+  controls: string[];
+}[] = [
+  {
+    city: "Минск",
+    center: [53.902735, 27.555696],
+    zoom: 11,
+    controls: ["zoomControl", "fullscreenControl"],
+  },
+  {
+    city: "Брест",
+    center: [52.093754, 23.685107],
+    zoom: 12,
+    controls: ["zoomControl", "fullscreenControl"],
+  },
+  {
+    city: "Витебск",
+    center: [55.184217, 30.202878],
+    zoom: 13,
+    controls: ["zoomControl", "fullscreenControl"],
+  },
+  {
+    city: "Гомель",
+    center: [52.435159, 31.019465],
+    zoom: 12,
+    controls: ["zoomControl", "fullscreenControl"],
+  },
+  {
+    city: "Гродно",
+    center: [53.674757, 23.840581],
+    zoom: 13,
+    controls: ["zoomControl", "fullscreenControl"],
+  },
+  {
+    city: "Могилев",
+    center: [53.894548, 30.330654],
+    zoom: 12,
+    controls: ["zoomControl", "fullscreenControl"],
+  },
+];
 
 export const DealersPage: FC = () => {
-  const [fetchPage, { data: pageData, isLoading }] =
+  const [fetchPage, { data: responseData, isLoading, isSuccess }] =
     useLazyFetchDealersPageDataQuery();
-
-  useEffect(() => {
-    fetchPage();
-  }, []);
-
-  function scrollToElement(elementId: string) {
-    var element = document.getElementById(elementId);
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  }
-
+  const [filterCity, setFilterCity] = useState<string>("Без фильтра");
+  const [pageData, setPageData] = useState<DealersPageT>();
   const [zoom, setZoom] = useState(6);
+  const [mapRefresh, setMapRefresh] = useState<{
+    center: number[];
+    zoom: number;
+    controls: string[];
+  }>();
 
   const mapState = useMemo(
     () => ({
@@ -35,13 +109,88 @@ export const DealersPage: FC = () => {
     [zoom],
   );
 
-  const [mapRefresh, setMapRefresh] = useState<{
-    center: number[];
-    zoom: number;
-    controls: string[];
-  }>();
+  useEffect(() => {
+    setFilterCity("Без фильтра");
+    fetchPage();
+  }, []);
 
-  const handleMapRotate = ([latitude, longitute]: number[]) => {
+  useEffect(() => {
+    if (responseData && isSuccess) {
+      setPageData(responseData);
+    }
+  }, [responseData]);
+
+  const handleFilter = (event: ChangeEvent<HTMLInputElement>, city: string) => {
+    if (pageData && responseData) {
+      if (city !== "Без фильтра") {
+        setFilterCity(city);
+        const filteredData = responseData.body.content.filter(
+          (dealer) => dealer.city === city,
+        );
+        setPageData(() => ({
+          ...responseData,
+          body: {
+            ...responseData.body,
+            content: filteredData,
+          },
+        }));
+      } else {
+        setFilterCity("Без фильтра");
+        setPageData(responseData);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (filterCity && mapEndpoints) {
+      if (filterCity === "Гродно") {
+        const newState = mapEndpoints.filter((map) => map.city === "Гродно")[0];
+        setMapRefresh(newState);
+        scrollToElement("yandex-map");
+      } else if (filterCity === "Минск") {
+        const newState = mapEndpoints.filter((map) => map.city === "Минск")[0];
+        setMapRefresh(newState);
+        scrollToElement("yandex-map");
+      } else if (filterCity === "Брест") {
+        const newState = mapEndpoints.filter((map) => map.city === "Брест")[0];
+        setMapRefresh(newState);
+        scrollToElement("yandex-map");
+      } else if (filterCity === "Витебск") {
+        const newState = mapEndpoints.filter(
+          (map) => map.city === "Витебск",
+        )[0];
+        setMapRefresh(newState);
+        scrollToElement("yandex-map");
+      } else if (filterCity === "Могилев") {
+        const newState = mapEndpoints.filter(
+          (map) => map.city === "Могилев",
+        )[0];
+        setMapRefresh(newState);
+        scrollToElement("yandex-map");
+      } else if (filterCity === "Гомель") {
+        const newState = mapEndpoints.filter((map) => map.city === "Гомель")[0];
+        setMapRefresh(newState);
+        scrollToElement("yandex-map");
+      } else if (filterCity === "Без фильтра") {
+        const newState = {
+          center: [53.67, 28.04],
+          zoom: 6,
+          controls: ["zoomControl", "fullscreenControl"],
+        };
+        setMapRefresh(newState);
+        scrollToElement("yandex-map");
+      }
+    }
+  }, [filterCity]);
+
+  function scrollToElement(elementId: string) {
+    var element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
+  const handleMapRotate = ([latitude, longitute]: number[], city: string) => {
     const newState = {
       center: [latitude, longitute],
       zoom: 14,
@@ -60,6 +209,75 @@ export const DealersPage: FC = () => {
       <HeaderSlider image={pageData.body.page_header.image} />
       <div className={styles.dealersWrapper_title}>
         <h1>{pageData.body.page_header.title}</h1>
+        <ThemeProvider theme={theme}>
+          <FormControl
+            sx={{
+              width: "100%",
+              marginBottom: "-30px",
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "2rem 1rem",
+            }}
+          >
+            <FormLabel
+              id="controlled-radio-buttons-group"
+              sx={{ color: "black", ":focus": "none", fontWeight: "bold" }}
+            >
+              Дилеры по городам:
+            </FormLabel>
+            <RadioGroup
+              aria-labelledby="controlled-radio-buttons-group"
+              name="controlled-radio-buttons-group"
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                flexDirection: "row",
+                paddingLeft: "0.5rem",
+              }}
+              value={filterCity}
+              onChange={handleFilter}
+            >
+              <FormControlLabel
+                value="Без фильтра"
+                control={<Radio />}
+                label="Вся Беларусь"
+              />
+              <FormControlLabel
+                value="Минск"
+                control={<Radio />}
+                label="Минск"
+              />
+              <FormControlLabel
+                value="Витебск"
+                control={<Radio />}
+                label="Витебск"
+              />
+              <FormControlLabel
+                value="Брест"
+                control={<Radio />}
+                label="Брест"
+              />
+              <FormControlLabel
+                value="Гомель"
+                control={<Radio />}
+                label="Гомель"
+              />
+              <FormControlLabel
+                value="Гродно"
+                control={<Radio />}
+                label="Гродно"
+              />
+              <FormControlLabel
+                value="Могилев"
+                control={<Radio />}
+                label="Могилев"
+              />
+            </RadioGroup>
+          </FormControl>
+        </ThemeProvider>
       </div>
       <div className={styles.dealersWrapper_content}>
         <div className={styles.dealersWrapper_content_leftSide}>
@@ -77,7 +295,11 @@ export const DealersPage: FC = () => {
                 {dealer.image_logo && (
                   <img
                     src={`http://93.177.124.158/media/${dealer.image_logo}`}
-                    style={{ width: "60%", objectFit: "contain" }}
+                    style={
+                      index + indexAll === 0
+                        ? { width: "60%", objectFit: "contain", paddingTop: 0 }
+                        : { width: "60%", objectFit: "contain" }
+                    }
                     alt={"dealer logo"}
                   />
                 )}
@@ -153,7 +375,10 @@ export const DealersPage: FC = () => {
                   />
                   <CommonButton
                     onClick={() =>
-                      handleMapRotate([branch.latitude, branch.longitude])
+                      handleMapRotate(
+                        [branch.latitude, branch.longitude],
+                        dealer.city,
+                      )
                     }
                     text={"Показать на карте"}
                   />
@@ -179,15 +404,15 @@ export const DealersPage: FC = () => {
               {pageData.body.content.map((dealer, index) =>
                 dealer.branches.map((branch, indexBranch) => (
                   <Placemark
-                    key={index + indexBranch + "_placemark"}
-                    defaultGeometry={[branch.latitude, branch.longitude]}
+                    key={`${index}_${indexBranch}`}
+                    geometry={[branch.latitude, branch.longitude]}
                     properties={{
                       balloonContentBody: `${branch.name} ${dealer.name}: ${branch.address}`,
                       hintContent: `<b>${branch.name} ${dealer.name}</b>`,
                     }}
                     options={{
                       preset: "islands#icon",
-                      iconColor: "red", // цвет иконки, можно также задавать в hex
+                      iconColor: "red",
                     }}
                   />
                 )),
