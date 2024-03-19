a85346959f9f5049624e98c61731eb9697c9b143
aleksejpetrovec <rbtechologics@mail.ru>
2024-03-19 10:29:18 +0300

    pdfViewer fix for old browsers;

--- src/components/pdfViewer/index.tsx
+++ src/components/pdfViewer/index.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 export const PDFViewer: FC = () => {
   const pdfURL = `https://dev.shineray.by/media/uploads/${window.location.href.split("uploads/")[1]}`;
   return (
-    <div style={{ width: "100dvw", height: "100dvh" }}>
+    <div style={{ width: "100vw", height: "100vh" }}>
       <iframe
         src={pdfURL}
         width="100%"
