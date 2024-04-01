0d6cd9ff68dd5b2063a14e04cb633e999d101ec4
aleksejpetrovec <rbtechologics@mail.ru>
2024-04-01 09:31:40 +0300

    deployment changes;

--- src/components/Preloader/index.tsx
+++ src/components/Preloader/index.tsx
@@ -3,7 +3,19 @@ import "./Preloader.css";
 
 export const Preloader: FC = () => {
   return (
-    <div className="preloaderWrapper">
+    <div
+      className="preloaderWrapper"
+      style={{
+        position: "fixed",
+        left: "0",
+        bottom: "0",
+        right: "0",
+        top: "0",
+        backgroundColor: "#ffffff",
+        zIndex: "98",
+        transition: "ease-in-out 0.3s",
+      }}
+    >
       <div className="loader rspin">
         <span className="c"></span>
         <span className="d spin">
