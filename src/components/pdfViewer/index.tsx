7e94d59424ec47eba1d3b1b4a09ab79a91493267
aleksejpetrovec <rbtechologics@mail.ru>
2024-04-02 13:25:41 +0300

    fix;

--- src/components/pdfViewer/index.tsx
+++ src/components/pdfViewer/index.tsx
@@ -4,11 +4,16 @@ export const PDFViewer: FC = () => {
   const pdfURL = `https://shineray.by/media/uploads/${window.location.href.split("uploads/")[1]}`;
 
   return (
-    <div style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+    <div style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "flex-start" }}>
       <object
         data={pdfURL}
         type="application/pdf"
-        style={{ width: "100%", height: "100%" }}
+        style={{
+          width: "calc(100vw - 40px)", // Adjust padding as needed
+          height: "auto",
+          maxWidth: "100%", // Prevent horizontal overflow
+          overflow: "hidden", // Hide any excess content
+        }}
         title={"Shineray-PDF-viewer"}
       >
         PDF файлы не поддерживаются вашим браузером. Пожалуйста загрузите файл на ваше устройство, для просмотра.
