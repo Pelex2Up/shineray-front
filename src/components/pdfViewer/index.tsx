8d20107d309428e8395191f564eff8c14ce12e32
aleksejpetrovec <rbtechologics@mail.ru>
2024-04-02 13:18:57 +0300

    pdfViewer changes;

--- src/components/pdfViewer/index.tsx
+++ src/components/pdfViewer/index.tsx
@@ -2,14 +2,17 @@ import { FC } from "react";
 
 export const PDFViewer: FC = () => {
   const pdfURL = `https://shineray.by/media/uploads/${window.location.href.split("uploads/")[1]}`;
+
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <iframe
-        src={pdfURL}
-        width="100%"
-        height="100%"
+    <div style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <object
+        data={pdfURL}
+        type="application/pdf"
+        style={{ width: "100%", height: "100%" }}
         title={"Shineray-PDF-viewer"}
-      />
+      >
+        PDF файлы не поддерживаются вашим браузером. Пожалуйста загрузите файл на ваше устройство, для просмотра.
+      </object>
     </div>
   );
 };
